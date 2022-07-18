6efcf31100e0f042103b5b7f9f49dde91330084f
Monday <monday@gmail.com>
2022-07-18 09:32:28 +0530

    These are some java basics

---
+++ first.java
@@ -0,0 +1,9 @@
+public class first {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      System.out.println("My name is nedsa");
+      System.out.println("My name is nana banthu");
+      System.out.println(9+3);
+      System.out.println("9+3");
+    }
+  }
